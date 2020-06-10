cd7a6fccc1853f0ba8d4159ab1dafeac50d234db
JustSomeoneOnGithub <59625582+JustSomeoneOnGithub@users.noreply.github.com>
2020-06-10 17:54:11 -0400

    Delete p.js

--- commands/p.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-const client= new Discord.Client();
-const config = require('../config.json');
-
-exports.run = (client, message, args) => {
-    message.author.send(`${message.author.toString()}`).catch(() => 
-        message.reply("Can't send DM to your user! \nEnable your DMs!\n(If you have me blocked me, unblock plz).")
-    );
-}
